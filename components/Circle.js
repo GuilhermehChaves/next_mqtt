996ec9dd65f3db841253f405a1af2053c7640b0c
GuilhermehChaves <contato.guilhermehchaves@gmail.com>
2020-11-28 23:01:44 -0300

    fix(className): fixing className word

--- components/Circle.js
+++ components/Circle.js
@@ -2,8 +2,8 @@ export default function Circle(props) {
     return (
         <a href={props.href} title={props.title}>
             <div className={`circle ${props.className}`}>
-            <p id={props.id} class="value">{props.value}</p>
-                <p class="title">
+            <p id={props.id} className="value">{props.value}</p>
+                <p className="title">
                     {props.children}
                     {props.value_name}
                 </p>
