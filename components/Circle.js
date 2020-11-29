b85fcc9aaa93c32e97e6fbc73499040873f638d9
GuilhermehChaves <contato.guilhermehchaves@gmail.com>
2020-11-28 21:09:51 -0300

    feat(values): update values

--- components/Circle.js
+++ components/Circle.js
@@ -2,7 +2,7 @@ export default function Circle(props) {
     return (
         <a href={props.href} title={props.title}>
             <div className={`circle ${props.className}`}>
-                <p id={props.id} class="value">0</p>
+            <p id={props.id} class="value">{props.value}</p>
                 <p class="title">
                     {props.children}
                     {props.value_name}
